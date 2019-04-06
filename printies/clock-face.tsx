5bd7affddb876e36ddd5a7bb8e3a9d593bda029f
Aleksey Bykov <aleksey.bykov@gmail.com>
2019-04-06 17:08:13 -0400

    tiny cleanup

--- printies/clock-face.tsx
+++ printies/clock-face.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 export function thusClockFace(radius: number) {
-    return class ClockFace extends React.Component<{}> {
+    return class ClockFace extends React.Component {
         render() {
             const digitSize = radius * 0.25;
             const innerRadius = radius - digitSize + digitSize * 0.2;
