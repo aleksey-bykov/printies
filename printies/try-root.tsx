0fe61efe1fbb598073d9895fe41389370d381047
Aleksey Bykov <aleksey.bykov@gmail.com>
2019-04-13 18:24:54 -0400

    non negative arithmetic problems

--- printies/try-root.tsx
+++ printies/try-root.tsx
@@ -6,16 +6,19 @@ import { Mid, Mid1, Tip } from './value-space';
 const random = new Randomizer(10);
 const mid = new Mid(
     0, 10, random,
-    (_value, _min, _max, random) =>
+    (value, _min, _max, random) =>
         new Mid1(['-', '+'], random, _operation =>
-            new Tip(0, 10, random)
+            new Tip(0, value, random)
         )
 );
 
+let count = 0;
 while (mid.hasMore()) {
     const [left, [operation, right]] = mid.draw();
     console.log(left, operation, right);
+    count += 1;
 }
+console.log(count);
 
 class App extends React.Component {
     render() {
