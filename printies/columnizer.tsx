4caf806a9175eeab453d21811ab90419bb084544
Aleksey Bykov <aleksey.bykov@gmail.com>
2019-04-13 21:14:24 -0400

    look for remainder last row cells

--- printies/columnizer.tsx
+++ printies/columnizer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { chunkHorizontally } from './arrays';
+import { chunkHorizontally, times } from './arrays';
 
 export interface ColumnizerProps {
     columns: number;
@@ -9,12 +9,18 @@ export class Columnizer extends React.Component<ColumnizerProps> {
     render() {
         const { stuff, columns } = this.props;
         const rows = chunkHorizontally(stuff, columns);
+        const size = rows.length > 0 ? rows[0].length : 0;
         return <div className="rows">
             {rows.map((row, rowIndex) =>
                 <div key={rowIndex} className="row">
                     {row.map((cell, cellIndex) =>
                         <div key={rowIndex + '-' + cellIndex} className="cell">{cell}</div>
                     )}
+                    {
+                        row.length < size
+                            ? times(size - row.length).map(() => <div className="cell as-empty"></div>)
+                            : null
+                    }
                 </div>
             )}
         </div>
